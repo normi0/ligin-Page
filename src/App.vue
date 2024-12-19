dbb8f433ea804e22d3d9cfc2db538d694a3330c9
normi0 <seif23achi@gmail.com>
2024-12-19 22:53:53 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <signupform/>
+</template>
+
+<script>
+import signupform from './components/SignUpForm.vue'
+export default {
+  name:'app',
+  components:{
+    signupform
+  },
+ 
+}
+</script>
+
+<style>
+#app{
+  font-family: Arial, Helvetica, sans-serif ;
+  text-align: center;
+  margin-top: 60px;
+}
+body{
+  margin: 0;
+}
+</style>
---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,80 @@
+<template>
+    <form class="signupform">
+        <label>Email:</label>
+        <input type="Email" required v-model="email">
+
+        <label>password:</label>
+        <input type="password" required v-model="password">
+        
+        <label>phonenumber:</label>
+        <input type="phonenumber" required v-model="phonenumber">
+        
+        <label>Role:</label>
+        <select class="selector">
+            <option value="developer">Web Dev</option>
+            <option value="Designer">Web Designer</option>
+        </select>
+
+        <div class="terms">
+            <input type="checkbox" reauired>
+            <label>Accept terms and Conditions</label>
+        </div>
+        
+    </form>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email:'',
+            password:'',
+            phonenumber:'',
+            role:'',
+        }
+    },
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style>
+   
+  form{
+    max-width: 420px;
+    margin: 30px auto;
+    background-color: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+    box-shadow: 0 5px 10px lightgrey;
+  }
+  label{
+     color: lightgrey;
+     display: inline-block;
+     margin: 25px 0 15px;
+     font-size: 0.6 em;
+     text-transform: uppercase;
+     letter-spacing: 1px;
+     font-weight: bold;
+  }
+  input ,select{
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid lightgrey;
+    color: gray;
+  }
+  input[type='checkbox']{
+    display: inline-block;
+    width: 16px;
+    margin: 0 10px 0 0 ;
+    position: relative;
+    top: 2px;
+  }
+</style>
